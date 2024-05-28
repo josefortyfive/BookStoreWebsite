74a42f9733b335b8a049667b7d29d2a87dd76943
josefortyfive <jose.fortyfive@gmail.com>
2024-05-28 20:59:18 +0800

    Added Annation to Book.java

--- src/main/java/com/bookstore/entity/Book.java
+++ src/main/java/com/bookstore/entity/Book.java
@@ -6,11 +6,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-
+@Entity
+@Table(name="book")
 public class Book implements java.io.Serializable {
 
 	private static final long serialVersionUID = 1L;
