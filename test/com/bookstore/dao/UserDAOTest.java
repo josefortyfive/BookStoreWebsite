3b337080c34117f8e2da29d094a355cafc6a7c4d
josefortyfive <jose.fortyfive@gmail.com>
2024-06-02 10:10:44 +0800

    Added test case for updating Users and test case if empty input

--- test/com/bookstore/dao/UserDAOTest.java
+++ test/com/bookstore/dao/UserDAOTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 
 import com.bookstore.entity.Users;
 
+import jakarta.persistence.PersistenceException;
+
 public class UserDAOTest {
 	private static UserDAO userDAO;
 	
@@ -20,9 +22,9 @@ public class UserDAOTest {
 	@Test
 	public void testCreateUsers() {
 		Users user1 = new Users();
-		user1.setEmail("john3@gmail.com");
-		user1.setFullName("John Smith");
-		user1.setPassword("password1");
+		user1.setEmail("");
+		user1.setFullName("");
+		user1.setPassword("");
 
 		
 		user1 = userDAO.create(user1);
@@ -30,7 +32,28 @@ public class UserDAOTest {
 		assertTrue(user1.getUserId() > 0);
 		
 	}
+	
+	@Test(expected = PersistenceException.class)
+	public void testCreateUsersFieldNotSet() {
+		Users user1 = new Users();
+		user1 = userDAO.create(user1);
+	}
 
+	@Test
+	public void testUpdateUsers() {
+		Users user1 = new Users();
+		user1.setUserId(1);
+		user1.setEmail("edmar@java.net");
+		user1.setFullName("Edmar Canin");
+		user1.setPassword("mysecret");
+		
+		user1 = userDAO.update(user1);
+		String expected = "mysecret";
+		String actual = user1.getPassword();
+		
+		assertEquals(expected, actual);
+	}
+	
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		userDAO.close();
