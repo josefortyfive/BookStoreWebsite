ab915039b1babed5cdbdcd801711f361c72946e2
josefortyfive <jose.fortyfive@gmail.com>
2024-06-02 10:04:31 +0800

    Added test case for adding user

--- test/com/bookstore/dao/UserDAOTest.java
+++ test/com/bookstore/dao/UserDAOTest.java
@@ -6,19 +6,34 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class UserDAOTest {
+import com.bookstore.entity.Users;
 
+public class UserDAOTest {
+	private static UserDAO userDAO;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		userDAO = new UserDAO();
+	}
+
+
+	@Test
+	public void testCreateUsers() {
+		Users user1 = new Users();
+		user1.setEmail("john3@gmail.com");
+		user1.setFullName("John Smith");
+		user1.setPassword("password1");
+
+		
+		user1 = userDAO.create(user1);
+		
+		assertTrue(user1.getUserId() > 0);
+		
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+		userDAO.close();
 	}
 
 }
