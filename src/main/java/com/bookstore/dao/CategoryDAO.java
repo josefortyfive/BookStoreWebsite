85d5fb01b7a883142524997a0ff98c86390e3bcf
josefortyfive <jose.fortyfive@gmail.com>
2024-06-03 18:16:29 +0800

    added CategoryDAO

---
+++ src/main/java/com/bookstore/dao/CategoryDAO.java
@@ -0,0 +1,45 @@
+package com.bookstore.dao;
+
+import java.util.List;
+
+import com.bookstore.entity.Category;
+
+public class CategoryDAO extends JpaDAO<Category> implements GenericDAO<Category>{
+
+	@Override
+	public Category create(Category t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Category update(Category t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Category get(Object id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Object id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Category> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
